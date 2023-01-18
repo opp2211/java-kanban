43fb1f17b7e30eb651ca877d1170b63a3db6d8ae
Kirill Maltsev <opp.2211@gmail.com>
2023-01-18 05:39:06 +0500

    refactor: Переделать ArrayList на Set при обновлении статуса эпика

--- src/tasks/EpicTask.java
+++ src/tasks/EpicTask.java
@@ -1,7 +1,9 @@
 package tasks;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class EpicTask extends Task {
     protected ArrayList<Integer> subTaskIds;
@@ -17,30 +19,13 @@ public class EpicTask extends Task {
     }
 
     public void updateStatus(Map<Integer, SubTask> subTasks) {
-        /* Комметарий по ревью:
-            мне понравилось предложенное Вами решение по обновлению статуса Эпика (переопределить метод getStatus),
-             я поппытался его реализовать, но возникла проблема:
-            - т.к. мне порекомендовали хранить в Эпиках не сами подзадачи, а их айдишники, то я не могу рассчитать статус
-            эпика из самого эпика, тк сам Эпик не имеет доступа к его подзадачам. Перегружать метод и передавать все саб-
-            таски через параметр метода мне показалось глупо, учитывая то что откроется возможность нечаянно вызвать
-            геттер предка (Task) и получить непересчитанный статус.
-
-            В прежнем подходе я использовал метод setStatus(...) предка (Task) для установки значения в менеджере, но
-            согласно вашему следующему критическому комментарию я не должен оставлять возможность изменить статус эпика 
-            в обход пересчета статусов подзадач.
-
-            Согласно ТЗ: имя, описание и статус задач не должны изменяться отдельно (они обновляются по новому объекту
-            целиком), в связи с чем я убрал сеттеры для этих полей и вынужден объявить этот специальный метод для Эпика
-            и по-прежнему вызывать его при каждом изменении саб-тасков
-         */
 
         if (getSubTaskIds().isEmpty()) {
             status = TaskStatus.NEW;
             return;
         }
 
-        ArrayList<TaskStatus> subTaskStatuses = new ArrayList<>(); /* Я не разобрался с коллекцией Set еще, посмотрю
-                                            попозже, но вроде бы она должна быть у нас в курсе через пару спринтов */
+        Set<TaskStatus> subTaskStatuses = new HashSet<>();
         for (Integer subTaskId : getSubTaskIds()) {
             TaskStatus subTaskStatus = subTasks.get(subTaskId).getStatus();
             subTaskStatuses.add(subTaskStatus);
